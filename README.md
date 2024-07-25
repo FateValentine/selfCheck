cd78f6ee04b34117462ac32c5cfbc90a1eb5690c
FateValentine <166237747+FateValentine@users.noreply.github.com>
2024-07-26 07:18:39 +0800

    J dev (#4)
    * Update index.html
    
    * 072624
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Jovi Joshua Tania <jovijoshuatania027@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# selfcheck
+# selfcheckkkkkkkkkkkkkkkkk
 
 This is a selfCheck todo-list from hell for busy individuals
 
-HAHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHA
+HAHAHAHAHA
--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Homeeeeeeeeeeeeeeeeeeeeeeeeee</RouterLink>
+        <RouterLink to="/about">Abouuuuuuuuuuuuuuuuuuuuuut</RouterLink>
       </nav>
     </div>
   </header>
